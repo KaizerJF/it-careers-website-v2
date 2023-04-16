1ccc6fda942595d51d9763a5dc42a40e9cdbf99e
KaizerJF <67563320+KaizerJF@users.noreply.github.com>
2023-04-16 10:31:40 +0000

    Added functions for application submission.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -28,6 +28,16 @@ def show_job(id):
 def show_job_json(id):
   job = load_job_from_db(id)
   return jsonify(job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
